0aa8c13dee9644152a2b0b859fd18180a060f462
User <User@DESKTOP-1364VBG>
2024-01-29 17:29:44 +0330

    Add project files.

---
+++ Sqrt/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.Write("Enter x : ");
+double x = Convert.ToDouble(Console.ReadLine());
+double x1 = +(x);
+double x2 = +(x);
+double x3 = +(x);
+double jam_1 = x2 + x3;
+
+double sqrt_1 = Math.Sqrt(jam_1);
+double jam_2 = x1 + sqrt_1;
+double sqrt_2 = Math.Sqrt(jam_2);
+Console.WriteLine(sqrt_2);
